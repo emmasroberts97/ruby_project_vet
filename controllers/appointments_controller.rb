0052850568bcdd1974ff8a2e59a82175ded7ebb4
Emma Roberts <emma.roberts197@gmail.com>
2020-02-03 14:26:28 +0000

    Added appointments and owners

--- controllers/appointments_controller.rb
+++ controllers/appointments_controller.rb
@@ -1,6 +1,7 @@
 require('sinatra')
 require('sinatra/contrib/all')
 require( 'pry-byebug' )
+require('date')
 require_relative( '../models/pet.rb' )
 require_relative('../models/appointment.rb')
 also_reload( '../models/*' )
@@ -14,4 +15,9 @@ post '/appointments' do
   @appointment = Appointment.new(params)
   @appointment.save()
   redirect to('/pets')
+end
+
+get '/appointments' do
+  @appointments = Appointment.all()
+  erb(:"appointments/index")
 end
